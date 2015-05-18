e6c26017456a6576961a3a12c609e96b54569e3d
Omar Rizwan <omar.rizwan@gmail.com>
2015-05-17 21:52:15 -0700

    move most of the drawing (<rect> tag, etc) into the user

--- dagre-graph.jsx
+++ dagre-graph.jsx
@@ -10,23 +10,113 @@
         root.DagreGraph = factory(root, modules[0], modules[1]);
     }
 })(this,
-   typeof require === 'function' ?
+   (typeof require === 'function' ?
        [require('react'), require('dagre')] :
-       [React, dagre],
+       [React, dagre]
+   ),
 function(window, React, dagre) {
     'use strict';
 
-    var DagreGraph = React.createClass({
+    var Graph = React.createClass({
+        /*
+           node = {
+               key: string
+               label: string
+           }
+           this.props.nodes = [node]
+
+           edge = [key1, key2]
+           this.props.edges = [edge]
+        */
         render: function() {
-            return <div>foo</div>;
+            // construct a new graph from scratch
+            // because dagre mutates g for the layout
+            var g = new dagre.graphlib.Graph();
+
+            g.setGraph({});
+
+            React.Children.forEach(this.props.children, function(child) {
+                if (child.type === Vertex) {
+                    console.log(child);
+
+                    g.setNode(child.key, { label: child, width: child.props.width, height: child.props.height });
+
+                } else if (child.type === Edge) {
+                    g.setEdge(child.props.source, child.props.target, { label: child });
+                }
+            });
+
+            dagre.layout(g);
+
+            // now render
+            var arrow;
+            if (arrow in this.props) {
+                arrow = this.props.arrow;
+
+            } else {
+                arrow = "<marker id=\"markerArrow\" markerWidth=\"6\" markerHeight=\"4\" \
+                             refx=\"5\" refy=\"2\" orient=\"auto\"> \
+                             <path d=\"M 0,0 V 4 L6,2 Z\" class=\"arrow\" /> \
+                         </marker>";
+            }
+
+            // node svg elements
+            var nodes = g.nodes().map(function(v) {
+                var node = g.node(v);
+                return React.cloneElement(node.label, {
+                    x: node.x,
+                    y: node.y
+                });
+
+            });
+            var edges = g.edges().map(function(e) {
+                var edge = g.edge(e);
+                return React.cloneElement(edge.label, {
+                    
+                });
+            });
+
+            return (
+                <svg {...this.props}>
+                    <defs dangerouslySetInnerHTML={{__html: arrow}} />
+                    {nodes}
+                    {edges}
+                </svg>
+            );
+        }
+    });
+    
+    var Vertex = React.createClass({
+        render: function() {
+            return (
+                <g transform={"translate("+
+                   (this.props.x-(this.props.width/2))+","+
+                   (this.props.y-(this.props.height/2))+")"}
+                   {...this.props}>
+
+                    {this.props.children}
+                </g>
+            );
         }
     });
 
+    var Edge = React.createClass({
+        render: function() {
+            return <path />;
+        }
+    });
+
+    var exports = {
+        Graph: Graph,
+        Vertex: Vertex,
+        Edge: Edge
+    };
+
     if (typeof module === 'undefined') {
-        window.DagreGraph = DagreGraph;
+        window.DagreGraph = exports;
     } else {
-        module.exports = DagreGraph
+        module.exports = exports;
     }
 
-    return Foo;
-})();
+    return exports;
+});
--- example/example.jsx
+++ example/example.jsx
@@ -1,6 +1,37 @@
+var Graph = DagreGraph.Graph;
+var Vertex = DagreGraph.Vertex;
+var Edge = DagreGraph.Edge;
+
 var Example = React.createClass({
     render: function() {
-        return <DagreGraph />;
+        var toVertex = function(name) {
+            return (
+                <Vertex width={50} height={50}
+                        key={name}>
+                    <rect width={50} height={50} />
+                    <text>
+                        {name}
+                    </text>
+                </Vertex>
+            );
+        };
+
+        return (
+            <Graph width="500" height="500">
+                {["bar", "baz"].map(toVertex)}
+                <Vertex width={70} height={30}
+                        key={"foo"}
+                        className="foo">
+                    <rect width={70} height={30} />
+                    <text>
+                        {"foo"}
+                    </text>
+                </Vertex>
+
+                <Edge source="foo" target="baz" />
+                <Edge source="bar" target="baz" />
+            </Graph>
+        );
     }
 });
 
